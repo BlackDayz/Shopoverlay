9caaf50694edc885b129949eacd8f564d51773db
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-05 21:41:13 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,202 @@
+<!--
+    *   Idee und geschrieben von BlackDayz
+    *   
+    *   Copyright 2020 BlackDayz
+    *
+    *   Das Verändern und Kopieren der Webiste ist nicht gestatet!
+    *
+    *   Anfragen an produce@blackdayz.de oder über Social Media (Instagram) @blackdayz_de
+    *
+    *   Der Shop ist nicht real! Dies ist nur ein Vorzeige Porjekt!
+    *
+
+    ***BlackDayz | Video Creator und Entwickler***
+
+-->
+
+<!DOCTYPE html>
+
+<html lang="de">
+
+    <head>
+
+        <title>BlackDayz &bull; Shop</title>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="copyright" content="BlackDayz &copy; 2020">
+        <meta http-equiv="content-language" content="de">
+        <meta name="author" content="BlackDayz &copy;" />
+        <meta name="robots" content="index, follow" />
+        <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="31536000">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Ein HTML, CSS, Javascript Overlay für einen Shop - justforfun">
+        <meta name="keywords" content="justforfun, shop, onlineshop">
+
+        <link rel="stylesheet" href="src/css/style.css">
+        <link rel="stylesheet" href="src/css/special.css">
+        <link rel="stylesheet" href="src/css/nav.css">
+        <link rel="stylesheet" href="src/css/Home.css">
+        <link rel="stylesheet" href="src/css/footer.css">
+
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+        <link rel="stylesheet" href="https://use.typekit.net/xue1nky.css">
+        <link rel="stylesheet" href="https://use.typekit.net/xue1nky.css">
+
+
+        <link rel="shortcut icon" href="https://www.blackdayz.de/src/img/logo/shortcuticon.png">
+        <link rel="apple-touch-icon" href="https://www.blackdayz.de/src/img/logo/shortcuticon.png" />
+
+        <script src="https://use.fontawesome.com/fb37175af0.js" rel="preload"></script>
+
+    </head>
+
+    <body class="font">
+
+        
+		<?php include 'src/inc/nav.html';?>
+
+        <div class="wrapper relative">
+
+            <section class="home-page-1 margin-0-auto">
+                <img src="src/img/Startseite/Neuerscheinungen/Neuerscheinung.jpeg" class="width-100 height-auto">
+            </section>
+
+            <section class="home-page-2 margin-0-auto display-inline-block center height-auto width-100 white">
+                <div class="hp2-left-info left">
+                    <div class="hp2-header">
+                        <i class="fa fa-trophy" aria-hidden="true"></i>
+                    </div>
+                    <div class="hp2-itext">
+                        Lorem ipsum dolor sit amet consectetur, a
+                        dipisicing elit. Beatae repellendus aliquam porro repreh
+                        enderit sint et neque necessitatibus aliquid veniam possimus
+                        fuga nisi officiis aut rem quod ut corporis, labore ab!
+                    </div>
+                </div>
+
+                <div class="hp2-left-info left">
+                    <div class="hp2-header">
+                        <i class="fa fa-bolt" aria-hidden="true"></i>
+                    </div>
+                    <div class="hp2-itext">
+                        Lorem ipsum dolor sit amet consectetur, a
+                        dipisicing elit. Beatae repellendus aliquam porro repreh
+                        enderit sint et neque necessitatibus aliquid veniam possimus
+                        fuga nisi officiis aut rem quod ut corporis, labore ab!
+                    </div>
+                </div>
+
+                <div class="hp2-right-info right">
+                    <div class="hp2-header">
+                        <i class="fa fa-paypal" aria-hidden="true"></i>
+                    </div>
+                    <div class="hp2-itext">
+                        Lorem ipsum dolor sit amet consectetur, a
+                        dipisicing elit. Beatae repellendus aliquam porro repreh
+                        enderit sint et neque necessitatibus aliquid veniam possimus
+                        fuga nisi officiis aut rem quod ut corporis, labore ab!
+                    </div>
+                </div>
+            </section>
+
+            <section class="home-page-3 w3-display-container margin-0-auto">
+                <img class="home-page-3-slideshow w3-animate-right width-100"
+                    src="https://www.freundin.de/sites/freundin.de/files/styles/media_small_m_x2/public/images/2019-11/new-york-str-fashiontrend-2020-t.jpg?itok=BrwIX160">
+                <img class="home-page-3-slideshow w3-animate-left width-100"
+                    src="https://www.blackdayz.de/src/img/logo/Blackdayzlogo.png">
+                <img class="home-page-3-slideshow w3-animate-left width-100"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR7MfoMjAgS0hkhY668aBHWYSlxEl6zPTA7_g&usqp=CAU">
+            </section>
+
+
+
+            <section class="home-page-4 margin-0-auto height-auto width-100 display-inline-block center white">
+                <div class="hp4-left-info left">
+                    <div class="hp4-header">
+                        <i class="fa fa-info-circle" aria-hidden="true"></i>
+                    </div>
+                    <div class="hp4-itext">
+                        Lorem ipsum dolor sit amet consectetur, a
+                        dipisicing elit. Beatae repellendus aliquam porro repreh
+                        enderit sint et neque necessitatibus aliquid veniam possimus
+                        fuga nisi officiis aut rem quod ut corporis, labore ab!
+                    </div>
+                </div>
+
+                <div class="hp4-left-info left">
+                    <div class="hp4-header">
+                        <i class="fa fa-question-circle" aria-hidden="true"></i>
+                    </div>
+                    <div class="hp4-itext">
+                        Lorem ipsum dolor sit amet consectetur, a
+                        dipisicing elit. Beatae repellendus aliquam porro repreh
+                        enderit sint et neque necessitatibus aliquid veniam possimus
+                        fuga nisi officiis aut rem quod ut corporis, labore ab!
+                    </div>
+                </div>
+
+                <div class="hp4-right-info right">
+                    <div class="hp4-header">
+                        <i class="fa fa-money" aria-hidden="true"></i>
+                    </div>
+                    <div class="hp4-itext">
+                        Lorem ipsum dolor sit amet consectetur, a
+                        dipisicing elit. Beatae repellendus aliquam porro repreh
+                        enderit sint et neque necessitatibus aliquid veniam possimus
+                        fuga nisi officiis aut rem quod ut corporis, labore ab!
+                    </div>
+                </div>
+            </section>
+
+
+        </div>
+
+        <!--!Footer-->
+
+        <div class="footer relative white tiny-2 height-auto width-100 display-inline-block">
+            <div class="footer-top">
+                <div class="footer-more left center list-style-type-n">
+                    <ul>
+                        <li class="bold big red">Spezielles</li>
+                        <li><a href="#">Spezielle&nbsp;Angebote</a></li>
+                        <li><a href="#">Angebote</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-sitemap center left list-style-type-n">
+                    <ul>
+                        <li class="bold big red">Sitemap</li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="https://www.blackdayz.de/Datenschutz.html">Datenschutz</a></li>
+                        <li><a href="https://www.blackdayz.de/Datenschutz.html">Impressum</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-partner right center list-style-type-n weight-100">
+                    <ul>
+                        <li class="bold big red"><a href="#">Unsere Partner</a></li>
+                        <li><a href="#">DHL</a></li>
+                        <li><a href="#">DEF&nbsp;Shop</a></li>
+                        <li><a href="https://www.blackdayz.de/">BlackDayz.de</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="footer-bottom center">
+                <div class="brand center list-style-type-n">
+                    <ul>
+                        <li>&copy;&nbsp;Design&nbsp;und&nbsp;Development&nbsp;by&nbsp;<a
+                                href="https://www.blackdayz.de/">BlackDayz.de</a></li>
+                        <li>Alle Rechte vorbehalten</li>
+                        <li class="red bold">Dieser Shop ist nicht echt! Er ist nur aus Spaß erstellt worden</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <script src="src/js/slideshow.js"></script>
+    </body>
+
+</html>
